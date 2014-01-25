f2925a6358744bb6cabd9a0da511c81e11fa44cc
Weiran (Ryan) Zhao <zhaoweiranregister@gmail.com>
2014-01-25 16:55:21 -0500

    There are bugs in original libwb library, I encounter this in mp6, so copy and paste latest virsion from their public github

--- wbCUDA.cpp
+++ wbCUDA.cpp
@@ -1,44 +1,28 @@
 
 #include "wb.h"
-
 #ifdef WB_USE_CUDA
-typedef struct st_wbCUDAMemory_t {
-    void * mem;
-    size_t sz;
-} wbCUDAMemory_t;
 
-static wbCUDAMemory_t _cudaMemoryList[1024];
-static int idx = 0;
+int _cudaMemoryListIdx = 0;
 
 size_t _cudaMallocSize = 0;
 
-cudaError_t wbCUDAMalloc(void ** devPtr, size_t sz) {
-    cudaError_t err = cudaMalloc(devPtr, sz);
-    if (idx == 0) {
-        memset(_cudaMemoryList, 0, sizeof(wbCUDAMemory_t) * 1024);
-    }
-    _cudaMallocSize += sz;
-    _cudaMemoryList[idx].mem = *devPtr;
-    _cudaMemoryList[idx].sz = sz;
-    idx++;
-    return err;
+wbCUDAMemory_t _cudaMemoryList[_cudaMemoryListSize];
+
+
+char * wbRandom_list(size_t sz) {
+  size_t ii;
+  char * rands = wbNewArray(char, sz);
+  int * irands = (int *) rands;
+  for (ii = 0; ii < sz/sizeof(int); ii++) {
+    irands[ii] = rand();
+  }
+  ii = sizeof(int)*(sz/sizeof(int));
+  while (ii < sz) {
+    rands[ii] = (char) (rand() % 255);
+    ii++;
+  }
+  return rands;
 }
 
-cudaError_t wbCUDAFree(void * mem) {
-    if (idx == 0) {
-        memset(_cudaMemoryList, 0, sizeof(wbCUDAMemory_t) * 1024);
-    }
-    for (int ii = 0; ii < idx; ii++) {
-        if (_cudaMemoryList[ii].mem != NULL &&
-                _cudaMemoryList[ii].mem == mem) {
-            cudaError_t err = cudaFree(mem);
-            _cudaMallocSize -= _cudaMemoryList[ii].sz;
-            _cudaMemoryList[idx].mem = NULL;
-            return err;
-        }
-    }
-    return cudaSuccess;
-}
 
 #endif /* WB_USE_CUDA */
-
